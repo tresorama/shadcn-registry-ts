c88485b243693316a9f8d0c95e25764435f10bfd
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-07-29 01:27:38 +0200

    Merge branch 'chore/fix-build-4' - fix deployment url instead of production url
    Squashed commit of the following:
    
    commit c3f324ef4e28a6cb2479f844da64e6f47dcf495f
    Author: Jacopo Marrone <jacopo.marrone27@gmail.com>
    Date:   Tue Jul 29 01:26:37 2025 +0200
    
        chore: fix wrong production url on Vercel
    
         // - VERCEL=1 if deployed on vercel
          // - VERCEL_GIT_COMMIT_REF:
          //     - i.e. `main`
          //     - is the git branch of the deployment
          // - VERCEL_URL:
          //     - i.e. `my-project-4535c432342x4234.vercel.app`
          //     - is the immutable deplyment url.
          //     - changes on each deployment
          // - VERCEL_PROJECT_PRODUCTION_URL:
          //     - i.e. `my-project.vercel.app`
          //     - is the fixed production url of the project on Vercel
          //     - does not change across deployments
          //     - if custom domain is used, this is the custom domain
          //     - if no custom domain is used, this is the auto-assigned by vercel production url
    
        Because we used `VERCEL_URL` as `APP_BASE_URL`, the deployment url wa used (https://shadcn-registry-ie16l2n7g-jacopo-marrones-projects.vercel.app)
        But we wanted the production url (https://shadcn-registry-ts.vercel.app).
    
        - for production deploy -> Use `VERCEL_PROJECT_PRODUCTION_URL` when git branch is `main`
        - for preview deploy -> Use `VERCEL_URL` when git branch is NOT `main`

--- next-app/src/constants/utils/get-vercel-url.ts
+++ next-app/src/constants/utils/get-vercel-url.ts
@@ -2,8 +2,34 @@
 /** Get deploy vercel url if this app is built on vercel, or null otherwise */
 export const getVercelUrl = () => {
 
-  if (process.env.VERCEL === '1' && process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+  // NOTE:
+  // - VERCEL=1 if deployed on vercel
+  // - VERCEL_GIT_COMMIT_REF:
+  //     - i.e. `main`
+  //     - is the git branch of the deployment
+  // - VERCEL_URL:
+  //     - i.e. `my-project-4535c432342x4234.vercel.app`
+  //     - is the immutable deplyment url.
+  //     - changes on each deployment
+  // - VERCEL_PROJECT_PRODUCTION_URL:
+  //     - i.e. `my-project.vercel.app`
+  //     - is the fixed production url of the project on Vercel
+  //     - does not change across deployments
+  //     - if custom domain is used, this is the custom domain
+  //     - if no custom domain is used, this is the auto-assigned by vercel production url
+
+  // if we are on vercel...
+  if (process.env.VERCEL === '1') {
+
+    // ...and this git branch is main and we have a vercel production url...
+    if (process.env.VERCEL_GIT_COMMIT_REF === 'main' && process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+      return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+    }
+
+    // ...and this git branch is not main and we have a vercel deployment url...
+    if (process.env.VERCEL_GIT_COMMIT_REF !== 'main' && process.env.VERCEL_URL) {
+      return `https://${process.env.VERCEL_URL}`;
+    }
   }
 
   return null;
