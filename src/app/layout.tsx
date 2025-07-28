5123028568c9d11f1c2724ab9994ae25e7010de8
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-07-28 17:05:25 +0200

    chore: add `GlobalNextServerDataProvider` to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./tailwind.css";
 
+import { GlobalNextServerDataProvider } from "@/components/views/global-next-server-data/server-component";
+
 // fonts files
 
 const geistSans = Geist({
@@ -33,7 +35,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <GlobalNextServerDataProvider>
+          {children}
+        </GlobalNextServerDataProvider>
       </body>
     </html>
   );
