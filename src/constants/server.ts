4e717eb458e081514dd2078bb405ad0bad070181
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-07-28 16:36:39 +0200

    chore: create `lib/registry/get-next-data` for getting data from `registry/output`
    ## Background
    
    In previou commits, we created an intermediate step, that using `registry/input/registry.input.json` as input, generates json files for rendering the next app (sidebar, registry items pages, search input ).
    
    **We never used**
    
    ## What this commit does
    
    This commit create the code used by the next app to read that data.
    
    **Changes**
    - add `getData` object, used to read the json used as input to derive the rest
    - add constant `APP_DATA_MODE`:
        - `static` means that `getData` read files from disk, this means that you need to manually run `pnpm registry:generate-static-data` before starting next `pnpm dev` and every time you update `registry.input` you muts re run the command or next has stale data
        -`compute` means that `getData` generate static files on the fly, so changes to `registry.input` are reflected while the dev server is running. Prefer this.
    - create `getRegistryItemNames` used for generating `static path` for SSG
    - create `getRegistryItemByName` ued for rendering single registry item page
    - create `getSidebarData` used for sidebar and search box

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -2,13 +2,15 @@ import z from "zod";
 
 const envSchema = z.object({
   APP_BASE_URL: z.string(),
+  APP_DATA_MODE: z.enum(['static', 'compute']),
 });
 
 export const {
   APP_BASE_URL,
+  APP_DATA_MODE
 } = envSchema.parse(process.env);
-
 
 // console.log({
 //   APP_BASE_URL,
+//   APP_DATA_MODE
 // });
