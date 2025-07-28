be331f0d136aa7791c953c8863e718014f6d7bdf
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-07-28 16:13:42 +0200

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import { ViewHome } from "@/views/home";
+import type { Metadata } from "next";
 
-// This page displays items from the custom registry.
-// You are free to implement this with your own design as needed.
+import { LinkWithIconExternal } from '@/components/mine/link-with-icon-external';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 export default function Page() {
-  return <ViewHome />;
+  return (
+    <div className="w-full h-full px-6 py-10 flex flex-col justify-center items-center text-center">
+      <h1 className="text-4xl lg:text-6xl font-normal tracking-tight">Typescript Utilities</h1>
+      <p className="mt-4 text-lg/relaxed text-muted-foreground">
+        Collection of typescript utilities, served as a shadcn registry.
+        <br />
+        <span className="text-sm">
+          By <LinkWithIconExternal href="https://github.com/tresorama">tresorama</LinkWithIconExternal>
+        </span>
+      </p>
+      <p className="mt-6 text-sm/loose text-muted-foreground">
+        Big thanks to <LinkWithIconExternal href="https://ui.shadcn.com/">shadcn</LinkWithIconExternal> and <LinkWithIconExternal href="https://nextjs.org/">next.js</LinkWithIconExternal>.
+      </p>
+    </div>
+  );
 }
+
+
+
