31bbcd96e15870e3d9d27e4725ed9c5b62e01d84
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-09-06 20:24:16 +0200

    feat: `array` - create `createArrayWithLength`

--- next-app/registry/input/items/utility/array.example.md
+++ next-app/registry/input/items/utility/array.example.md
@@ -4,5 +4,8 @@ import { getLastArrayItem } from "./array"
 // getLastArrayItem(any[]) => any
 getLastArrayItem([0, 100, 50]); // 50
 
+// createArrayWithLength
+createArrayWithLength(2); // ['_, '_']
+
 ```
 
--- next-app/registry/input/items/utility/array.test.ts
+++ next-app/registry/input/items/utility/array.test.ts
@@ -1,12 +1,21 @@
 import { describe, it, expect } from 'vitest';
 
-import { getLastArrayItem } from './array';
+import { getLastArrayItem, createArrayWithLength } from './array';
 
 describe('array - getLastArrayItem', () => {
 
   it('do it', () => {
     expect(getLastArrayItem([])).toBeUndefined();
     expect(getLastArrayItem([1, 2, 3])).toBe(3);
+  });
+
+});
+
+describe('array - createArrayWithLength', () => {
+
+  it('do it', () => {
+    expect(createArrayWithLength(3).length).toBe(3);
+    expect(createArrayWithLength(2).map((_, i) => i)).toEqual([0, 1]);
   });
 
 });
