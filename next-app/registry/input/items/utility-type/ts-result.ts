53737b4316a1fe1f7d353199a4411b339daa1ad0
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-09-06 16:19:05 +0200

    feat: `ts-result` - update
    ## Changes
    - allow to pass error codes to error path
    - add infer types utilities

--- next-app/registry/input/items/utility-type/ts-result.ts
+++ next-app/registry/input/items/utility-type/ts-result.ts
@@ -3,16 +3,36 @@
  */
 
 
-type ResultSuccess<T> = {
+/** Create a Result Success type */
+type ResultSuccess<S> = {
   status: 'success';
-  data: T;
+  data: S;
 };
-type ResultError = {
+/** Create a Result Error type */
+type ResultError<Code extends string> = {
   status: 'error';
+  code: Code | 'UNKNOWN_ERROR',
   message: string;
 };
-/** The generic you pass will be the data prop of success */
-export type Result<T> = ResultSuccess<T> | ResultError;
 
-/** The generic you pass will be the data prop of success. There is no error allowed output. Always success. */
-export type ResultAlwaysSuccess<T> = ResultSuccess<T>;
+/** 
+ * Create a Result type.  
+ * You must pass two generics:
+ * - the first will be the `data` prop of `success` path.
+ * - the second will be the `code` prop of `error` path.
+ * */
+export type Result<S, E extends string> = ResultSuccess<S> | ResultError<E>;
+
+/** 
+ * Create a Result type that cannot have `error` path.
+ * You must pass one generic:
+ * - will be the `data` prop of `success` path.
+ * */
+export type ResultAlwaysSuccess<S> = ResultSuccess<S>;
+
+
+/** Utility used to infer the `success` discriminated union of a Result type */
+export type InferResultSuccess<R> = Extract<R, { status: 'success'; }>;
+
+/** Utility used to infer the `error` discriminated union of a Result type */
+export type InferResultError<R> = Extract<R, { status: 'error'; }>;
