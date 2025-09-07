5d5ade170cd2bf9220a5aeeb392981291b2860a1
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-09-07 13:51:14 +0200

    chore: refactor dotenv files + constants

--- next-app/src/constants/server.ts
+++ next-app/src/constants/server.ts
@@ -2,18 +2,17 @@ import z from "zod";
 
 import { getVercelUrl } from "./utils/get-vercel-url";
 
-const envSchema = z.object({
-  APP_BASE_URL: z.string(),
+const envVars = z.object({
   APP_DATA_MODE: z.enum(['static', 'compute']),
-});
+}).parse(process.env);
 
 export const {
   APP_BASE_URL,
   APP_DATA_MODE
-} = envSchema.parse({
-  ...process.env,
+} = {
+  APP_DATA_MODE: envVars.APP_DATA_MODE,
   APP_BASE_URL: getVercelUrl() || "http://localhost:3000",
-});
+};
 
 // console.log({
 //   APP_BASE_URL,
