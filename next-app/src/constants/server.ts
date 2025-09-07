3e9b9c1291ad1e2549ce52973052d5ff8265c6db
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-09-07 13:56:09 +0200

    chore: constants - add `DEBUG_CONSTANTS` env var and constant

--- next-app/src/constants/server.ts
+++ next-app/src/constants/server.ts
@@ -3,13 +3,16 @@ import z from "zod";
 import { getVercelUrl } from "./utils/get-vercel-url";
 
 const envVars = z.object({
+  DEBUG_CONSTANTS: z.enum(['true', 'false']).optional().default('false'),
   APP_DATA_MODE: z.enum(['static', 'compute']),
 }).parse(process.env);
 
 export const {
+  DEBUG_CONSTANTS,
   APP_BASE_URL,
   APP_DATA_MODE
 } = {
+  DEBUG_CONSTANTS: envVars.DEBUG_CONSTANTS,
   APP_DATA_MODE: envVars.APP_DATA_MODE,
   APP_BASE_URL: getVercelUrl() || "http://localhost:3000",
 };
