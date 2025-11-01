f1fec1bdeebcb71ea8066df11964855c7d33bb13
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-11-01 12:58:36 +0100

    chore: CodeNotCollapsible - add `className` prop

--- next-app/src/components/mine/code-not-collapsible/index.tsx
+++ next-app/src/components/mine/code-not-collapsible/index.tsx
@@ -1,4 +1,5 @@
 import { ButtonCopyToClipboard } from "@/components/mine/button-copy-to-clipboard";
+import { cn } from "@/lib/shadcn/utils";
 
 
 export type CodeNotCollapsibleProps = {
@@ -8,15 +9,18 @@ export type CodeNotCollapsibleProps = {
   codeJsx: React.ReactNode;
   /** text that will be copied to clipboard when clicking the copy button */
   codeStringForClipboard?: string;
+
+  className?: React.ComponentProps<'div'>['className'];
 };
 
 export const CodeNotCollapsibleServer = ({
   fileTitle,
   codeJsx,
   codeStringForClipboard,
+  className,
 }: CodeNotCollapsibleProps) => {
   return (
-    <Wrapper>
+    <Wrapper className={className}>
       {fileTitle ? (
         <Header
           fileTitle={fileTitle}
@@ -34,9 +38,10 @@ export const CodeNotCollapsibleServer = ({
 };
 
 
-const Wrapper = ({ children }: { children: React.ReactNode; }) => (
-  <div data-name="CODE-NOT-COLLAPSIBLE"
-    className="relative border rounded-xl overflow-hidden"
+const Wrapper = ({ children, className }: Pick<CodeNotCollapsibleProps, "className"> & { children: React.ReactNode; }) => (
+  <div
+    data-name="CODE-NOT-COLLAPSIBLE"
+    className={cn("relative border rounded-xl overflow-hidden", className)}
   >
     {children}
   </div>
