38103d77dadf8eedc114891674f5f9aaff2b03a0
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-11-01 16:36:40 +0100

    test: convertMarkdownToHTMLString - update test after `rehype-slug` addition

--- next-app/src/components/mine/markdown-renderer/utils/convert-markdown-to-html-string.test.ts
+++ next-app/src/components/mine/markdown-renderer/utils/convert-markdown-to-html-string.test.ts
@@ -59,7 +59,7 @@ const OUTPUT_HTML = `
       }
     </style>
     
-<h2>Contents</h2>
+<h2 id="contents">Contents</h2>
 <ul>
 <li><a href="#heading-1">Heading 1</a>
 <ul>
@@ -67,9 +67,9 @@ const OUTPUT_HTML = `
 </ul>
 </li>
 </ul>
-<h1>Heading 1</h1>
-<h2>Heading 2</h2>
-<h3>Heading 3</h3>
+<h1 id="heading-1">Heading 1</h1>
+<h2 id="heading-2">Heading 2</h2>
+<h3 id="heading-3">Heading 3</h3>
 <pre class="shiki shiki-themes github-light github-dark" style="background-color:var(--code);--shiki-dark-bg:var(--code);color:#24292e;--shiki-dark:#e1e4e8" tabindex="0" hello="world" title="prova" language="ts" code="const a = 1;"><code><span class="line"><span style="color:#D73A49;--shiki-dark:#F97583">const</span><span style="color:#005CC5;--shiki-dark:#79B8FF"> a</span><span style="color:#D73A49;--shiki-dark:#F97583"> =</span><span style="color:#005CC5;--shiki-dark:#79B8FF"> 1</span><span style="color:#24292E;--shiki-dark:#E1E4E8">;</span></span></code></pre>
 <ul>
 <li>item 1</li>
